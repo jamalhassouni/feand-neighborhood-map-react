9dabf5941de9675fc31a3d79fe1a2a5ccbb28363
jamal hassouni <jamalhassouni13@gmail.com>
2018-08-28 18:45:45 +0100

    feat: Add hamburg menu to Navbar components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Navbar.css';
 import Logo from '../../assets/imgs/logo.svg';
 
-const Navbar = () => (
+const Navbar = ({ navToggle }) => (
   <header className="navbar">
     <h1 className="navbar-title">
       <img src={Logo} alt="World with markers and paths in it" className="icon"/>
       <span>Neighborhood Map</span>
     </h1>
-  </header>
-);
+    <div className="nav-toggle"  onClick={e => navToggle(e)}>
+  <div className="nav-toggle-bar"></div>
+</div>
 
+  </header>
+
+);
+Navbar.propTypes = {
+  navToggle: PropTypes.func.isRequired
+}
 export default Navbar;
