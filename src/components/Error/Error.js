e9ce7d8bf8d53deb505a094862df39c54f04e170
jamal hassouni <jamalhassouni13@gmail.com>
2018-08-29 11:22:19 +0100

    feat: Create Error component to show error in map

---
+++ src/components/Error/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Error.css';
+import ErrorIcon from '../../assets/imgs/error.svg';
+
+const Error = ({ message }) => (
+  <div className="error">
+    <div className="message" role="alert">
+      <img src={ErrorIcon} alt="Red letter X"/>
+      <p>{ message }</p>
+    </div>
+  </div>
+);
+
+Error.propTypes = {
+  message: PropTypes.string.isRequired
+}
+
+export default Error;
