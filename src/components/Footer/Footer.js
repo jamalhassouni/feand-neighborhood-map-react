02a372180d7884c82b9535413c72ab529a3e6398
jamal hassouni <jamalhassouni13@gmail.com>
2018-08-30 14:21:34 +0100

    fix: Fix footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,11 +9,6 @@ const Footer = () => (
       <img src={ReactIcon} alt="React" title="React"/>
       by <a href="https://github.com/jamalhassouni" target="_blank" rel="noopener noreferrer">Jamal Hassouni</a>
     </p>
-    <div className="icons">
-      Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>
-        and  <a href="https://www.iconfinder.com/korawan_m" title="BomSymbols">BomSymbols</a>
-
-    </div>
   </footer>
 );
 
